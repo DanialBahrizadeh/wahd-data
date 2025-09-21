80f406f3c40f24325314d3a55422e60c05369598
danial <danialmessi477@gmail.com>
2025-09-21 23:50:09 +0330

    fixed the delete and simplifed the get function

--- src/db/memory.ts
+++ src/db/memory.ts
@@ -1,32 +1,26 @@
-import { createClient, RedisClientType } from "redis";
-import MemoryStore from "./store";
+import MemoryStore from "../models/store";
 
 class Memory implements MemoryStore {
+  public name: string = "memory";
+  private map: Map<string, string>;
 
-    public name: string = "memory";
-    private map: Map<string, string>;
+  constructor() {
+    this.map = new Map();
+  }
 
-    constructor() {        
-        this.map = new Map();
-    };
+  async get(key: string): Promise<string | null> {
+    return this.map.get(key) || null;
+  }
 
-    async get(key: string): Promise<string | null> {
-        const result = this.map.get(key);
-        if(!result) {
-            return null;
-        } 
-        return result;
-    }
+  async set(key: string, value: string, _options?: any): Promise<boolean> {
+    this.map.set(key, value);
+    return true;
+  }
 
-    async set(key: string, value: string, options?: any): Promise<boolean> {
-        this.map.set(key, value);
-        return true;
-    };
-
-    async delete(key: string): Promise<boolean> {
-        return true
-    }
-
+  async delete(key: string): Promise<boolean> {
+    this.map.delete(key);
+    return true;
+  }
 }
 
 const memoryStore = new Memory();
--- src/db/redis.ts
+++ src/db/redis.ts
@@ -1,53 +1,49 @@
 import { createClient, RedisClientType } from "redis";
-import MemoryStore from "./store";
+import MemoryStore from "../models/store";
 import { env } from "../config/env";
 
 class Redis implements MemoryStore {
+  public name: string = "redis";
+  private client: RedisClientType;
 
-    public name: string = "redis";
-    private client: RedisClientType;
+  constructor(url: string) {
+    this.client = createClient({
+      url,
+    });
 
-    constructor(url: string) {
-        
-        this.client = createClient({
-            url
-        });
+    this.client.on("error", (error) => {
+      console.error("Redis client error", error);
+    });
 
-        this.client.on('error', error => {
-            console.error("Redis client error", error);
-        })
+    this.client.on("connection", (stream) => {
+      console.log("a connection was made");
+    });
+  }
 
-        this.client.on('connection', (stream) => {
-            console.log("a connection was made");
-        })
+  async connect() {
+    await this.client.connect();
+  }
 
-    };
+  async get(key: string): Promise<string | null> {
+    const result = await this.client.get(key);
+    return result;
+  }
 
-    async connect() {
-        await this.client.connect();
-    }
+  async set(key: string, value: string, options?: any): Promise<boolean> {
+    await this.client.set(key, value, options);
+    return true;
+  }
 
-    async get(key: string): Promise<string | null> {
-        const result = await this.client.get(key);
-        return result;
-    }
-
-    async set(key: string, value: string, options?: any): Promise<boolean> {
-        await this.client.set(key, value , options);
-        return true;
-    };
-
-    async delete(key: string): Promise<boolean> {
-        return true
-    }
-
+  async delete(key: string): Promise<boolean> {
+    await this.client.del(key);
+    return true;
+  }
 }
 
-const redisStore = new Redis(process.env.REDIS_URL!);
 const redisStore = new Redis(env.REDIS_URL);
 
 (async () => {
-    await redisStore.connect();
+  await redisStore.connect();
 })();
 
 export default redisStore;
